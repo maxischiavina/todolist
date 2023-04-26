cc6e2c3090854b67843ceed995e98f8c496a3c15
maxischiavina <maxischiavina@hotmail.com>
2023-04-26 10:10:16 +0000

    sumando funcionalidades

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareaIngresada, setTareaIngresada] = useState("");
+	const [lista, setLista] = useState("");
+	
+
+	function agregarTarea(event){
+		event.preventDefault()
+		const nuevaTarea = {id: Date.now(), texto: tareaIngresada};
+		setLista([...tareas, nuevaTarea]);
+		setTareaIngresada("");
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div classNameName="text-center">
+			<h1 classNameName="text-center mt-5">todos</h1>
+
+			<form onSubmit={agregarTarea}>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="task" onChange={(event)=> (setTareaIngresada(event.target.value), console.log(tareaIngresada) )} className="form-control" id="task" aria-describedby="emailHelp" />
+    <div id="task" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+			
 		</div>
 	);
 };
